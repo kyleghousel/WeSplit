aee96f57b4c8c952af0b6e516080dbce211c826b
kyleghousel <69773129+kyleghousel@users.noreply.github.com>
2023-11-14 14:58:06 -0500

    WeSplit final push
    Created first project following Hacking With Swift 's "100 Days of SwiftUI." Simple app that uses total of a bill, desired tip percentage, and amount of diners to calculate how much each individual diner owes, assuming they are splitting the bill evenly.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,15 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    @State var checkAmount = 0.0
+    @State var numberOfPeople = 2
+    @State var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 18, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
     }
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Form {
+                    Section {
+                        TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                            .keyboardType(.decimalPad)
+                            .focused($amountIsFocused)
+                        
+                        Picker("Number of people", selection: $numberOfPeople) {
+                            ForEach(2..<100) {
+                                Text("\($0) people")
+                            }
+                        }
+                    }
+                    
+                    Section(header: Text("How much would you like to tip?").font(.subheadline).foregroundColor(.black).headerProminence(.increased).italic()) {
+                        
+                        Picker("Tip percentage", selection: $tipPercentage) {
+                            ForEach(tipPercentages, id: \.self) {
+                                Text($0, format: .percent)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    
+                    Section(header: Text("Total per person").font(.headline).foregroundColor(.green).headerProminence(.increased).bold()) {
+                        Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    }
+                }
+            }
+            .navigationTitle("We$plit")
+            .navigationBarTitleDisplayMode(.large)
+            .font(.headline)
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
